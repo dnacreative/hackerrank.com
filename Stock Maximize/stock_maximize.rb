b23ad1cb5e9f41a185818b2d8cd0dbc107a5fe78
Ilias Fotopoulos <i.fotopoulos42@gmail.com>
2014-05-13 11:30:04 +0300

    Working (passed all test cases) but MESSY solution

--- Stock Maximize/stock_maximize.rb
+++ Stock Maximize/stock_maximize.rb
@@ -1,12 +1,35 @@
+def max_split(arr)
+	max = arr.max
+	c = 0 ; acc = 0
+	arr.delete_if do |v|
+		if v != max
+			acc += v ; c += 1 ;
+		else
+			profit = (max * c) - acc
+			return arr, profit
+		end
+	end
+end
+
+
+def profit(arr)
+	pro = 0
+	while arr.count > 1 do
+		arr, temp = max_split(arr) 
+		pro += temp
+		arr.delete_at(0)
+	end
+	return pro
+end
+
 T = gets.chomp.to_i
-output = Array.new(T)
+output = []
 forecast = []
 
 T.times do
     N = gets.chomp.to_i
     forecast = gets.chomp.split(" ").map!{ |v| v.to_i }
+    output << profit(forecast) 
 end
 
-def profit(arr)
-
-end
+puts output
