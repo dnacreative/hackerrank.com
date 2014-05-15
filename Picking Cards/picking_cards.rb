f1211c9e569b44ff996ef020c602de107f3a98f0
Ilias Fotopoulos <i.fotopoulos42@gmail.com>
2014-05-15 23:30:45 +0300

    1st try for solution

---
+++ Picking Cards/picking_cards.rb
@@ -0,0 +1,19 @@
+gets.chomp.to_i.times do
+    h = Hash.new(0)
+    result = 1 
+    
+    n = gets.chomp.to_i
+    arr = gets.chomp.split(" ").map(&:to_i)
+
+    arr.each do |v|
+        h[v] += 1
+    end
+    
+    0.upto(n-1) do |step|
+        result *= h[step]
+        h[step+1] += h[step] -1
+    end
+    puts result % 1000000007
+    
+end
+    
